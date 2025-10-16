b0c4a93ce661c73acce95dfa899cfdb9b07b9c87
Eli Mohr <69693818+EliMohr@users.noreply.github.com>
2025-10-16 10:26:41 -0500

    Update Assignment_4.py

--- Assignment_4.py
+++ Assignment_4.py
@@ -10,10 +10,12 @@ adjective2 = input ("Give me another adjective:  ")
 userWords = [verb1, verb2, noun1, noun2, adjective1, adjective2]
 
 text = "A luminous lantern let a tired traveler wander silently."
+
 new_text = text.replace("luminous", userWords[4])
 new_text = new_text.replace("lantern", userWords[2])
 new_text = new_text.replace("let", userWords[0])
 new_text = new_text.replace("tired", userWords[5])
 new_text = new_text.replace("traveler", userWords[3])
 new_text = new_text.replace("wander", userWords[1]) 
+
 print (new_text)
