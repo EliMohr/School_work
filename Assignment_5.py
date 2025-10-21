94930e31447d5d90d6d881d81eafc2c167b6a63e
Eli Mohr <69693818+EliMohr@users.noreply.github.com>
2025-10-21 11:26:41 -0500

    Refactor word collection and add previous input display
    Changed file writing mode to overwrite user words instead of appending, and ensured all user inputs are written to 'UserWords.txt' with newlines. Added functionality to read and display previous user inputs after generating the adlib story.

--- Assignment_5.py
+++ Assignment_5.py
@@ -1,21 +1,25 @@
 #Collecting words from the user
-txtFile = open("UserWords.txt", "a")
+txtFile = open("UserWords.txt", "w")
 
 verb1 = input ("Give me a verb:  ")
-txtFile.write(verb1)
-txtFile.close()
-verb2 = input ("Give me another verb:  ")
-#txtFile.write(verb2)
-noun1 = input ("Give me a noun:  ")
-#txtFile.write(noun1)
-noun2 = input ("Give me another noun:  ")
-#txtFile.write(noun2)
-adjective1 = input ("Give me an adjective:  ")
-#txtFile.write(adjective1)
-adjective2 = input ("Give me another adjective:  ")
-#txtFile.write(adjective2)
-
+txtFile.write(verb1 + "\n")
 #txtFile.close()
+verb2 = input ("Give me another verb:  ")
+txtFile.write(verb2 + "\n")
+#txtFile.close()
+noun1 = input ("Give me a noun:  ")
+txtFile.write(noun1 + "\n")
+#txtFile.close()
+noun2 = input ("Give me another noun:  ")
+txtFile.write(noun2 + "\n")
+#txtFile.close()
+adjective1 = input ("Give me an adjective:  ")
+txtFile.write(adjective1 + "\n")
+#txtFile.close()
+adjective2 = input ("Give me another adjective:  ")
+txtFile.write(adjective2 + "\n")
+txtFile.close()
+
 
 userWords = [verb1, verb2, noun1, noun2, adjective1, adjective2]
 #txtFile.write(str(userWords))
@@ -28,3 +32,14 @@ new_text = new_text.replace("tired", userWords[5])
 new_text = new_text.replace("traveler", userWords[3])
 new_text = new_text.replace("wander", userWords[1]) 
 print (new_text)
+
+#get the old words from the last time the user used our adlib app
+with open("UserWords.txt", "r") as txtFile:
+    lines = txtFile.readlines()
+    
+for line in lines:
+    print(line)
+
+#Insert our old words into the adlib
+print("Here is the old story without your new words:")
+print(text)
