d9284b9701d273cf8b03e3d29c1ccc1c574f8271
Eli Mohr <69693818+EliMohr@users.noreply.github.com>
2025-10-28 11:22:48 -0500

    Add MyClass with initialization method
    Introduces MyClass with an __init__ constructor to set Name, CourseCode, Section, and Objectives attributes.

---
+++ OOP_Practice.py
@@ -0,0 +1,6 @@
+class MyClass():
+    def __init__(self, Name, CourseCode, Section, Objectives):
+        self.Name = Name
+        self.CourseCode = CourseCode
+        self.Section = Section
+        self.Objectives = Objectives
